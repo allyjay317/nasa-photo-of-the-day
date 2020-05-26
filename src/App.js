c48bd58a43a06fc2bdb84a89222ea982826d7b07
Allison Usher <allyjay317@gmail.com>
2020-05-26 15:42:31 -0700

    blocked out html, loaded data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  const [nasaData, setNasaData] = useState("")
+
+  useEffect(()=>{
+    let appData = axios.get("https://api.nasa.gov/planetary/apod?api_key=7UAcwN8x1uVges3J52DVhhSvWTkkXOfi3FxL08b3")
+    .then(data =>
+      console.log(data)
+    )
+  }, [nasaData, setNasaData])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <header>
+        <h1>NASA Photo of the Day</h1>
+      </header>
+      <div className="img-container">
+        <p>{/*title of the image*/}</p>
+        <img src="#"></img>
+      </div>
+      <footer>copyright nasa</footer>
     </div>
   );
 }
