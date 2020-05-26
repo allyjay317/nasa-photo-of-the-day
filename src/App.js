db5833434b80279f323057f98ec865db70d31951
Allison Usher <allyjay317@gmail.com>
2020-05-26 16:53:51 -0700

    finished blocking out, added background, tested video functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from 'axios'
+import axios from 'axios';
+import invert from 'invert-color'
+import analyze from 'rgbaster';
+
 
 function App() {
   const [nasaData, setNasaData] = useState("")
-  
+  const [textColor, setTextColor] = useState("#FFFFFF");
   useEffect(()=>{
     
     let appData = axios.get("https://api.nasa.gov/planetary/apod?api_key=7UAcwN8x1uVges3J52DVhhSvWTkkXOfi3FxL08b3")
     .then(data => {
       console.log(data)
+      
       return setNasaData(data.data)
     })
   }, [])
@@ -18,14 +22,28 @@ function App() {
       <header>
         <h1>NASA Photo of the Day</h1>
       </header>
-      <div className="img-container" alt={nasaData.explanation}>
-        <h2>{nasaData.title}</h2>
-        <p>{nasaData.explanation}</p>
-        <img src={nasaData.hdurl}></img>
+      <div className="img-container" alt={`"${nasaData.title}": by ${nasaData.copyright}. Photo taken ${nasaData.date}`}>
+        <h2 color={textColor}>{nasaData.title}</h2>
+        <p color={textColor}>{nasaData.explanation}</p>
+        {getPhotoOfDay(nasaData.media_type, nasaData.url)}
       </div>
   <footer>Copyright {nasaData.copyright}</footer>
     </div>
   );
+}
+
+function getPhotoOfDay(type, url){
+  console.log(type, url)
+  if(type==="image"){
+    return <img src={url}></img>
+  }
+  else if(type ==="video"){
+    return (
+      <iframe 
+        src={`${url}&autoplay=1`} frameborder="0" 
+allowfullscreen></iframe>
+    )
+  }
 }
 
 export default App;
